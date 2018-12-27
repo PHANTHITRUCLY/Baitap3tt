fc0fb39f89a125a07ad2ee9980660b0cda5030bd
Phan Thị Trúc Ly <46109438+PHANTHITRUCLY@users.noreply.github.com>
2018-12-27 22:13:51 +0700

    bai3.de3.hoanthanh.text2+3

--- bai3.de3.cpp
+++ bai3.de3.cpp
@@ -19,37 +19,44 @@ struct Ngchoi{
 };
 vector<Ngchoi> nc;
 
+void Xuatngchoi(Ngchoi nc){
+	cout<<"\nTen nguoi choi: "<<nc.ten<<endl;
+	cout<<"\nDiem: "<<nc.diem<<endl;
+}
+void Xuatdsngchoi(){
+	size_t n = nc.size();
+	for(int i = 0; i < n; i++)
+	{
+		Xuatngchoi(nc[i]);
+	}			
+}
 
-void Luutenngchoi(){
-	Ngchoi ng;
+void Luutenngchoi(Ngchoi &ng,int diem){
+	
 	string s;
 	cout<<"\n Nhap ten nguoi choi: ";
+	fflush(stdin);
 	getline(cin,s);
 	ng.ten = s;
-	
-
-}
-
-void Luudiemvaoten(int diem){
-
-	Ngchoi ng;
 	ng.diem = diem;
-	cout<<"diem tong la: "<<diem<<endl;
-	Luutenngchoi();
 	nc.push_back(ng);
+	
 }
 
-
-void Docde(){ //doc file
-	int chonde;
-	cout<<"\n Nhap vao 1 or 2 or 3 tuong ung voi bo de muon chon: \n";
-	cin>>chonde;
-	
-	if(chonde == 1){
-		ifstream f("INPUT1.txt ");
-		
+void Xuatde(int so){
+	ifstream f;
+	 if(so == 1){
+	  f.open("INPUT1.txt ");	
+      }
+      else if(so == 2){
+	 f.open("INPUT2.txt ");	
+      }
+       else if(so == 3){
+	 f.open("INPUT3.txt ");	
+      }
 		string s,ans;
 		Cauhoi_dapan de;
+		Ngchoi an;
 		int sl; //sl cau hoi
 	    int diem=0;
 		getline(f,s); sl = atoi(s.c_str());
@@ -75,20 +82,48 @@ void Docde(){ //doc file
 				cout<<"\nBan tl dung!\n\n";
 				// cho nay de luu diem vao vector diem
 				 diem+=1;
-				 Luudiemvaoten(diem);
+			
+				 
 				//
 			}
 			else 
 				cout<<"\nBan tl sai!\n\n";
 			
 		}
-			
-	}
+	Luutenngchoi(an,diem);
+	Xuatngchoi(an);
 }
 
+void Docde(){ //mo file can doc
+	int chonde;
+	cout<<"\n Nhap vao 1 or 2 or 3 tuong ung voi bo de muon chon: \n";
+	cin>>chonde;
+	
+	if(chonde == 1){
+		
+		Xuatde(1);
+	}
+	else if (chonde == 2){
+		Xuatde(2);
+	}
+	else if (chonde == 3){
+		Xuatde(3);
+	}
+			
+	
+}
 
 int main(){
-	Docde();
+	int tam;
 	
+	do{	
+	cout<<"\nBan co muon choi ? (nhap 0 la dung): ";
+	cin>>tam;
+	if(tam!=0)
+	Docde();
+} while(tam!=0);
+	cout<<"\n Danh sach nguoi choi la: ";
+	Xuatdsngchoi();
+
 	getch();
 }
